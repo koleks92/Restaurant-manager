e58a5363de302de08bed75c406a9e9d6d0d4ec5e
koleks92 <koleks92@gmail.com>
2022-09-22 15:51:42 +0200

    started booking class, creating time slots, printing time slots

--- restaurant.py
+++ restaurant.py
@@ -1,6 +1,3 @@
-from unicodedata import name
-
-
 ## Simple restaurant manager to work on OOP in Python
 
 class Restaurant:
@@ -8,27 +5,61 @@ class Restaurant:
         self.name = name
 
     def print_name(self):
-        print(f"This is the {self.name} bookin manager\n Welcome !\n")
+        print(f"This is the {self.name} booking manager\nWelcome !\n")
 
     def tables(self):
-        tables = input("How many tables in the restaurant ?: ")
-        while tables not in '1234567890':
+        tables = int(input("How many tables in the restaurant ?: "))
+        while tables not in range(1,20):
             print("Please provide a proper number !")
-            tables = input("How many tables in the restaurant ?: ")
-        print("Tables saved in the system !")
-        return int(tables)
-
-    def guests(self):
+            tables = int(input("How many tables in the restaurant ?: "))
+        return list(range(1, int(tables)+1))
+        
+    def seats(self):
         seats = []
         print("Please provide amount of seats for each table !")
-        for i in range(int(tables)):
+        for i in range(len(tables)):
             seats.append(int(input(f"Table {i+1}: ")))
-        return sum(seats)
-
+        return seats
     
-azur = Restaurant("Coolio")
-azur.print_name()
-tables = azur.tables()
-print(f"There are {tables} tables in the restaurant")
-seats = azur.guests()
-print(f"There are {seats} seats available at {tables} tables.")
+    def seats_to_tables(self):
+        s2t = dict(zip(tables, seats))
+        return s2t
+    
+class Booking():
+
+    def __init__(self, name, books):
+        self.name = name
+        self.books = books
+
+    def time_slots(self):
+        time_slots = {}
+        times = ["17-19","19-21","21-23"]
+        for number in tables:
+            time_slots[number] = times
+        return time_slots
+
+    def print_time_slots(self):
+        print(f"\nTable {self.name+1} timeslots:")
+        print("| 17-19 | 19-21 | 21-23 |")
+        for b in self.books:
+            print("|%s| " % ("|".join(b)))
+    
+    def book_time(self):
+        self.books[0][2] = 'XXXXXXX'
+
+                        
+rest1 = Restaurant(input("Please provide the name of the restaurant: "))
+rest1.print_name()
+tables = rest1.tables()
+seats = rest1.seats()
+s2t = rest1.seats_to_tables()
+
+objs = []
+
+for i in range(len(tables)):
+    objs.append(Booking(i, [["       "] * 3 for i in range(1)]))
+    
+# print(objs[].print_time_slots())
+for i in range(len(objs)):
+    objs[i].print_time_slots()
+
