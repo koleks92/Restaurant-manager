9802e981fa5a99a97dac0e19f4ea8ee6255b689f
koleks92 <koleks92@gmail.com>
2022-10-14 23:33:29 +0200

    Guest menu, booking menu, comment

--- restaurant.py
+++ restaurant.py
@@ -6,6 +6,7 @@ TO DO
 -Make printing table more dynamic (fx if times changed, tables automaticly expands etc. !)
 -Screen clear !
 - Maybe GUI to practice ?
+- Remove booking, edit guest, 
 '''
 
 # Global variables
@@ -13,7 +14,7 @@ TO DO
 yes_no = ['yes', 'no', 'y', 'n']
 tables_list = []
 guest_list=[]
-times = ["17-19","19-21","21-23"]
+times = ["17-19","19-21","21-23"] ## Avaliable times to book a table
 
 
 def main():
@@ -26,10 +27,6 @@ def main():
     table_seats = rest1.seats() ## How many seates for each table
     s2t = rest1.seats_to_tables() ## Seats together with tables
     create_tables() ## Create tables
-    guests() ## Create guests
-
-    
-
     
 ## MENU 
     rest1.print_name()
@@ -52,25 +49,58 @@ def main():
                     s2t = rest1.seats_to_tables() ## Seats together with tables
                     create_tables() ## Create tables
                     print('New restaurant created !')
-                    input('Press any key to continue: ')
+                    input('Press ENTER to continue: ')
                     break
                 elif choice_3 == '1': ## Restart restaurant
                     create_tables()
                     print('Restaurant restarted !')
-                    input('Press any key to continue: ')
+                    input('Press ENTER to continue: ')
                     break
 
         elif choice_5 == '3': ## Print Time Table
             while True:
                 menu.time_table()
                 print(' ')
-                input('Press any key to continue: ')
+                input('Press ENTER to continue: ')
                 break
 
         elif choice_5 == '2': ## Booking system
-            break
+            while True:
+                menu.booking()
+                choice_3 = menu.menu_choice_3()
+                if choice_3 == '3': ## Go back to main menu
+                    break
+                elif choice_3 == '2': ## Remove booking
+                    break
+                elif choice_3 == '1': ## Book a table
+                    booking()
+                    input("Press ENTER to continue: ")
+
         elif choice_5 == '1': ## Guest managment
-            break
+            while True:
+                menu.guests()
+                choice_3 = menu.menu_choice_3()
+                if choice_3 == '3': ## Go back to main menu
+                    break
+                elif choice_3 == '2': ## Remove/edit guest
+                    while True:
+                        menu.guests_modify()
+                        choice_3 = menu.menu_choice_3()
+                        if choice_3 == '3': ## Go back to guest menu
+                            break
+                        elif choice_3 == '2': ## Remove a guest
+                            if len(guest_list) == 0: ## Check if there a guests to remove
+                                print('There are no guests !')
+                                input('Press ENTER to continue: ')
+                                break
+                            guest_remove()
+                        elif choice_3 == '1': ## Edit a guest
+                            break
+                elif choice_3 == '1': ## Add a guest
+                    new_guest()
+                    input('Press ENTER to continue: ')
+                    break
+
 
 
 class Menu:
@@ -85,6 +115,7 @@ class Menu:
         print("")
 
     def menu_choice_5(self):
+        '''Five choice menu'''
         menu_choice = input('Please choose a number: ')
         while menu_choice.isnumeric() is False and menu_choice not in '12345':
             print("Please provide a correct number (1-5) !")
@@ -92,6 +123,7 @@ class Menu:
         return menu_choice
 
     def menu_choice_3(self):
+        '''Three choice menu'''
         menu_choice = input('Please choose a number: ')
         while menu_choice.isnumeric() is False and menu_choice not in '123':
             print("Please provide a correct number (1-5) !")
@@ -99,33 +131,46 @@ class Menu:
         return menu_choice
     
     def guests(self):
+        '''Guests menu'''
         print("1.Add a new guest")
-        print("2.Remove guest")
+        print("2.Remove or edit a guest")
         print("3.Go back to Main Menu")
         print('')
 
+    def guests_modify(self):
+        '''Guest submenu - Edit or remove'''
+        print("1.Edit a guest")
+        print("2.Remove guest")
+        print("3.Go back to Guest Menu")
+        print('')
+
+
     def booking(self):
+        '''Booking menu'''
         print("1.Make a new booking")
         print("2.Remove a booking")
         print("3.Go back to Main Menu")
         print('')
 
     def time_table(self):
+        '''Printing Timetable'''
         for i,val in enumerate(tables_list):
             tables_list[i].print_time_slots()
 
     def restaurant(self):
+        '''Restaurant menu'''
         print("1.Restart restaurant !")
         print("2.Create a new restaurant !")
         print("3.Go back to Main menu")
         print('')
 
     def exit(self):
+        '''Exit program menu'''
         choice_exit = input("Are you sure you want to quit ?: ")
         while choice_exit.lower() not in yes_no:
             print("Please provide a valid answer(yes or no) !")
             choice_exit = input("Are you sure you want to quit ?: ")
-        if choice_exit.lower() == 'yes':
+        if choice_exit.lower() == 'yes' or 'y':
             quit()
         else:
             self.print_menu()
@@ -240,21 +285,38 @@ class Guest:
 
 def new_guest():
     '''New guest creating func'''
-    guest_name = input("Enter guest name: ")
+    guest_name = input("Enter guest name: ") ## Name
     guest_phone = input("Enter guest phone number: ")
-    while guest_phone.isnumeric() is False:
+    while guest_phone.isnumeric() is False: ## Phone number
         print('Please provide correct phone number')
         guest_phone = input("Enter guest phone number: ")
     
     guest_guests = input("Enter the number of guest: ")
-    while guest_guests.isnumeric() is False and int(guest_guests) > 10:
+    while guest_guests.isnumeric() is False: ## Number of guests
         print('Please provide a correct number !')
         guest_guests = input("Enter the number of guest: ")
-    guest_new= Guest(guest_name,int(guest_phone),int(guest_guests))
-    guest_new.guest_creation()
-    return guest_new
-            
+    while int(guest_guests) > 10: ## Check for maximum number of guestes
+        print('Maximum number of guests is 10 !')
+        guest_guests = input("Enter the number of guest: ")
+    guest_new = Guest(guest_name,int(guest_phone),int(guest_guests)) ## Create a guest
+    guest_list.append(guest_new)
+    guest_new.guest_creation() ## Print info
 
+def guest_remove():
+    '''Guest removal function'''
+    print_guest_list()
+    choice_guest = input('Please choose a guest: ') ## Guest choice to remove
+    while choice_guest.isnumeric() is False and choice_guest not in range(len(guest_list)):
+        choice_guest = input('Please choose a guest: ')
+    remove_choice = input(f"Do want to remove {guest_list[int(choice_guest) - 1].name} ?: ") ## Confirmation
+    while remove_choice not in yes_no:
+        remove_choice = input(f"Do want to remove {guest_list[int(choice_guest) - 1].name} ?: ")
+    if remove_choice.lower() == 'yes' or 'y':
+        guest_list.remove(guest_list[int(choice_guest) - 1]) ## Remove a gust from a list
+        print("Guest removed !")
+    else:
+        print('Guest not removed !')
+           
 def print_guest_list():
     '''Print guest list'''
     for i in range(len(guest_list)):
@@ -265,36 +327,8 @@ def create_tables():
     for i, val in enumerate(tables):
         tables_list.append(Table(i, [["       "] * len(times) for i in range(1)], s2t[i+1]))
 
-##Creating new guest:
-
-def guests(): 
+def booking():
     while True:
-        choice = input('Would you like to add a new guest ?: ')
-        while choice not in yes_no:
-            choice = input('Would you like to add a new guest ?: ')
-        if choice.lower() == 'no':
-            break
-        else:
-            for i in range(1):
-                new=new_guest()
-                guest_list.append(new)
-
-## Restaurant Main
-
-
-main()
-
-
-
-     
-## Booking table
-while True:
-    choice = input('Would you like to booke a table ?: ')
-    while choice not in yes_no:
-        choice = input('Would you like to book a table ?: ')
-    if choice.lower() == 'no':
-        break
-    else:
         fully_booked = True 
         for i, val in enumerate(tables_list): ## Check if fully booked
             if val.fully_booked() is not True:
@@ -334,6 +368,12 @@ while True:
                         time_choice = tables_list[table_choice - 1].book_check()
                         tables_list[table_choice - 1].book_table(time_choice)
                         break
+
+        
+
+main()
+
+
                 
             
 
